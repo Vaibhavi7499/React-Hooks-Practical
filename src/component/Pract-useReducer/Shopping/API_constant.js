6e7ca66b0b2860b4724743e2b014173a2b3dbca3
Vaibhavi7499 <vaibhavibodke13@gmail.com>
2024-08-06 00:00:12 +0530

    expense tracker

--- src/component/Pract-useReducer/Shopping/API_constant.js
+++ src/component/Pract-useReducer/Shopping/API_constant.js
@@ -1,4 +1,5 @@
 export let API_URL = {
 products : "http://localhost:8000/products",
-addtocart : "http://localhost:8000/addtocart"
+addtocart : "http://localhost:8000/addtocart",
+expensetracker : "http://localhost:8000/addexpense"
 } 
